6aab0164f35d1330d922b7abf6d8c56a4af27c73
José <alejose@hotmail.com>
2020-07-16 15:42:11 -0400

    Creo que termine

--- js.js
+++ js.js
@@ -1,8 +1,20 @@
 var clicks = 0;
 
+function mas2 () {
+	clicks += 1;
+}
+
 document.onclick = function() {
-    clicks += 1;
-    document.getElementById("clicks").innerHTML = clicks;
+	clicks += 1;
+
+	if (clicks > 20) {
+		document.getElementById("por2").style.display = "block";
+	}
+
+	if (clicks > 50) {
+		document.getElementById("h2").style.display = "block";
+	}
+	document.getElementById("clicks").innerHTML = clicks;
 
     var simbolos, color;
 	simbolos = "0123456789ABCDEF";
